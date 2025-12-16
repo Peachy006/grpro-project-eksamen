e2e4d2bb039330505039325499f8b92081cefcdd
William Noes-Rasmussen <wino@itu.dk>
2025-12-16 12:51:58 +0100

    PredatorTest næsten færdig, PackTest færdig

--- src/test/inherits/PredatorTests.java
+++ src/test/inherits/PredatorTests.java
@@ -6,8 +6,10 @@ import itumulator.world.World;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import project.animals.Bear;
+import project.animals.Carcass;
 import project.animals.Rabbit;
 import project.animals.Wolf;
+import project.inherits.Animal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,8 +69,12 @@ public class PredatorTests {
         Location wolfLocation = new Location(2, 2);
         Location bearLocation = new Location(2, 3);
 
+
         Wolf wolf = new Wolf(1,  false);
         Bear bear = new Bear(  false);
+
+        w.setTile(wolfLocation,wolf);
+        w.setTile(bearLocation,bear);
 
         wolf.setEnergy(100);
         bear.setEnergy(100);
@@ -95,5 +101,123 @@ public class PredatorTests {
         assertFalse(wolf1.canEat(wolf2, w));
 
     }
+
+    @Test
+    void DoesInteractWithNearbyAnimalsWork() {
+        Location wolfLocation = new Location(2, 2);
+        Location AnimalLocation = new Location(2, 3);
+
+        Wolf wolf1 = new Wolf(1, false);
+        Wolf wolf2 = new Wolf(1,  false);
+        Wolf wolf3 = new Wolf(2,  false);
+        Rabbit rabbit = new Rabbit(  false);
+        Rabbit rabbit1 = new Rabbit(  false);
+        Bear bear = new Bear(  false);
+        Bear bear1  = new Bear(  false);
+
+        //Testing with a Wolf and a Rabbit
+        w.setTile(wolfLocation,wolf1);
+        w.setTile(AnimalLocation,rabbit);
+
+        rabbit.setEnergy(30);
+        wolf1.setEnergy(50);
+
+        wolf1.interactWithNearbyAnimals(w,true);
+
+        assertEquals(80,wolf1.getEnergy());
+        assertFalse(w.getTile(AnimalLocation)instanceof Rabbit);
+
+
+        //Testing with two wolfs from the same pack
+        wolf2.setEnergy(50);
+        w.setTile(AnimalLocation,wolf2);
+
+        wolf1.interactWithNearbyAnimals(w,true);
+
+        assertEquals(50,wolf2.getEnergy());
+        assertTrue(w.getTile(AnimalLocation)instanceof Wolf);
+        w.delete(wolf2);
+
+        //Testing with two wolfs from different packs
+
+        wolf3.setEnergy(50);
+        w.setTile(AnimalLocation,wolf3);
+
+        wolf1.interactWithNearbyAnimals(w,true);
+
+        assertTrue(wolf3.getEnergy() < 50);
+        assertTrue(w.getTile(AnimalLocation)instanceof Wolf);
+        w.delete(wolf3);
+
+        //Testing with a wolf and a bear
+
+        w.setTile(AnimalLocation,bear);
+        bear.setEnergy(50);
+
+        wolf1.interactWithNearbyAnimals(w,true);
+        assertTrue(w.getTile(AnimalLocation)instanceof Bear);
+        assertTrue(bear.getEnergy() < 50);
+        w.delete(bear);
+
+        //Testing if wolf interact with Carcass
+
+        w.setTile(AnimalLocation,rabbit);
+        rabbit.killThisAnimal(w,true);
+
+        assertTrue(w.getTile(AnimalLocation)instanceof Carcass);
+
+        wolf1.interactWithNearbyAnimals(w,true);
+
+        assertFalse(w.getTile(AnimalLocation)instanceof Carcass);
+
+        //Testing if bear interacts with all animals around it when StopAfterFirstAction is false
+
+        Location bearLocation = new Location(3,3 );
+        Location rabbitLocation = new Location(3,2);
+        Location caracassLocation = new Location(4,2);
+        Location bear1Location = new Location(4,3);
+
+
+        w.setTile(AnimalLocation,wolf2);
+        w.setTile(bearLocation,bear);
+        w.setTile(rabbitLocation,rabbit);
+        w.setTile(caracassLocation,rabbit1);
+        w.setTile(bear1Location,bear1);
+
+        rabbit1.killThisAnimal(w,true);
+        //Wolf 1, Wolf 2, bear, bear1, rabbit, rabbit 1
+
+        wolf1.setEnergy(50);
+        wolf2.setEnergy(50);
+        bear1.setEnergy(50);
+
+        bear.interactWithNearbyAnimals(w,false);
+
+        assertTrue(wolf1.getEnergy() < 50);
+        assertTrue(wolf2.getEnergy() < 50);
+        assertTrue(bear1.getEnergy() < 50);
+        assertFalse(w.getTile(rabbitLocation)instanceof Rabbit);
+        assertFalse(w.getTile(caracassLocation)instanceof Carcass);
+
+
+
+    }
+
+    @Test
+    void DoesCanAttackWorks() {
+        Wolf wolf = new Wolf(1, false);
+        Wolf wolf1 = new Wolf(1,false);
+        Wolf wolf2 = new Wolf(2,false);
+        Bear  bear = new Bear(  false);
+        Rabbit rabbit =  new Rabbit(  false);
+
+        assertTrue(wolf.canAttack(wolf1,w));
+        assertTrue(wolf.canAttack(bear,w));
+
+        assertFalse(wolf.canAttack(rabbit,w));
+        assertFalse(wolf.canAttack(wolf2,w));
+        assertFalse(wolf.canAttack(wolf,w));
+    }
+
 
 }
