ff7408eec40df4d769500873bc42da29a94c8162
Mikkel Vincent Hedegaard <mikkel.vh11@gmail.com>
2025-12-17 10:19:59 +0100

    all structure tests and plant tests done, and with carcass too.

---
+++ src/test/plants/FungiTests.java
@@ -0,0 +1,71 @@
+package test.plants;
+
+import itumulator.executable.*;
+import itumulator.world.*;
+import project.animals.*;
+import project.plants.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class FungiTests {
+    private World world;
+    private Program program;
+
+    private Fungi fungi1;
+
+    Carcass carcass1;
+
+
+    @BeforeEach
+    void setUp() {
+        int size = 2;
+        int delay = 1000;
+        int display_size = 800;
+
+        program = new Program(size, display_size, delay);
+        world = program.getWorld();
+
+        fungi1 = new Fungi("large");
+        carcass1 = new Carcass(true,false);
+    }
+
+    @Test
+    void doesFungiSpread() {
+        world.setTile(new Location(0,0), fungi1);
+        world.setTile(new Location(1,0), carcass1);
+
+        fungi1.lookForOtherCarcassesToSpreadTo(world);
+
+        assertTrue(carcass1.hasFungi());
+    }
+
+    @Test
+    void doesFungiGrowGrass() {
+        world.setTile(new Location(0,0), fungi1);
+
+        HashMap<Object, Location> temp = (HashMap<Object, Location>) world.getEntities();
+        int count = 0;
+
+        for (Location loc : temp.values()) {
+            count++;
+        }
+
+        assertEquals(1, count);
+
+        while (count <= 1) {
+            fungi1.fungiTriesToGrowGrass(world,world.getLocation(fungi1));
+            temp = (HashMap<Object, Location>) world.getEntities();
+
+            for (Location loc : temp.values()) {
+                count++;
+            }
+        }
+
+        assertEquals(2, count);
+    }
+}
